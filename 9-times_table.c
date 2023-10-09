d097c16d620b99c64edae68c40cb24b5785db997
Z-ajamy <abdorahman0283@gmail.com>
2023-10-09 15:49:53 +0300

    100

--- 9-times_table.c
+++ 9-times_table.c
@@ -1,25 +1,69 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * times_table - prints the 9 times table, starting with 0
  *
- *
+ * Return (0) when success
  *
  */
 	void times_table(void)
-{ 
+{
 	int n = 0;
 
 	while (n <= 9)
 	{
 		int m = 0;
 
-		
 		while (m <= 9)
 		{
-			printf("%d,  ", n * m);
-		m++;
+		int	R = n * m;
+/* اول-عمود-اصفار*/
+		if (R == 0 && m == 0)
+		{
+			putchar(R + '0');
 		}
-		_putchar('\n');
+/**
+ * كل-الي-مكون-من
+ * -رقم--معادا-اول-2-في-العمود-الاخير
+ */
+		else if (R < 10 && m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(R + '0');
+			}
+/*كل-الي-مكون-من-رقمين--معادا-العمود-الاخير*/
+			else if (R >= 10 && m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(R / 10 + '0');
+				putchar(R % 10 + '0');
+			}
+			/*العمود-الاخير*/
+			else if (m == 9)
+			{
+			/*كل-العمود-عدا-اخر-2*/
+				if (R >= 10)
+				{
+				putchar(',');
+				putchar(' ');
+				putchar(R / 10 + '0');
+				putchar(R % 10 + '0');
+				}
+				/*اول-2-في-العمود*/
+				else if (R < 10)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(R % 10 + '0');
+				}
+			}
+			m++;
+		}
+		putchar('\n');
 		n++;
 	}
 }
